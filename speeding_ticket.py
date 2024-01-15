465c449f8590fd9cb51f2d62238f912b32d436c6
zhangzymia <156193264+zhangzymia@users.noreply.github.com>
2024-01-15 01:06:44 +0000

    Create speeding_ticket.py

---
+++ speeding_ticket.py
@@ -0,0 +1,18 @@
+def speeding_ticket(speed, is_birthday):
+    if is_birthday:
+        speed_limit_no_ticket = 65
+        speed_limit_small_ticket = 85
+    else:
+        speed_limit_no_ticket = 60
+        speed_limit_small_ticket = 80
+    if speed <= speed_limit_no_ticket:
+        return "No Ticket"
+    elif speed_limit_no_ticket < speed <=speed_limit_small_ticket:
+        return "Small Ticket"
+    else:
+        return "Big Ticket"
+print(speeding_ticket(60, False))
+print(speeding_ticket(75, False))
+print(speeding_ticket(85, False))
+print(speeding_ticket(65, True))
+print(speeding_ticket(85, True))
