29d0a6fda188ea5ecde56d5e977fd44757f258d7
zhangzymia <156193264+zhangzymia@users.noreply.github.com>
2024-01-27 17:38:09 +0000

    Update speeding_ticket.py

--- week1/speeding_ticket.py
+++ week1/speeding_ticket.py
@@ -7,7 +7,7 @@ def speeding_ticket(speed, is_birthday):
         speed_limit_small_ticket = 80
     if speed <= speed_limit_no_ticket:
         return "No Ticket"
-    elif speed_limit_no_ticket < speed <=speed_limit_small_ticket:
+    elif speed <=speed_limit_small_ticket:
         return "Small Ticket"
     else:
         return "Big Ticket"
